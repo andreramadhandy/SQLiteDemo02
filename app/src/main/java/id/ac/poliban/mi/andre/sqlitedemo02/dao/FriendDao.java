bb4b953d916f6e2bc69e7fa43b03797449153a80
andreramadhandy <andreramadhandy@gmail.com>
2020-01-07 21:22:15 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/andre/sqlitedemo02/dao/FriendDao.java
@@ -0,0 +1,17 @@
+package id.ac.poliban.mi.andre.sqlitedemo02.dao;
+
+import java.util.List;
+
+import id.ac.poliban.mi.andre.sqlitedemo02.domain.Friend;
+
+public interface FriendDao {
+    void insert(Friend f);
+
+    void update(Friend f);
+
+    void delete(int id);
+
+    Friend getFriendById(int id);
+
+    List<Friend> getAllFriends();
+}
